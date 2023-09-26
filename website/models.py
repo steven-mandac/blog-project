0c3bc418373ef39b4088039234083357ec0a6a25
Steven Lloyd Mandac <steven.mandac15@gmail.com>
2023-09-27 03:05:55 +0800

    [add] configure database models

---
+++ website/models.py
@@ -0,0 +1,9 @@
+from . import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
