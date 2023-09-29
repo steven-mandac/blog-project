5f3dc6ffc7d7f47b7d061aa1de2ceb1373377520
Steven Lloyd Mandac <steven.mandac15@gmail.com>
2023-09-30 01:02:15 +0800

    [add][edit][delete] added some styles, updated the layout's navigation bar and removed forms.py

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 
 auth = Blueprint('auth', __name__)
@@ -9,7 +9,7 @@ def login():
 
 @auth.route('/logout')
 def logout():
-    return render_template("index.html")
+    return redirect(url_for('views.home'))
 
 @auth.route('/sign-up')
 def signup():
--- website/models.py
+++ website/models.py
@@ -9,8 +9,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(250), unique=True, nullable=False)
     email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
-    birth_date = db.Column(db.String(150), default="dd-MM-YYYY")
-    posts = relationship('ShitPost')
+    posts = relationship('ShitPost', back_populates='user')
+    comments = relationship('Comment', back_populates='comment_author')
 
 
 class ShitPost(db.Model):
@@ -18,5 +18,18 @@ class ShitPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     post = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user = relationship('User', back_populates='posts')
     user_id = db.Column(db.ForeignKey('users.id'))
     
+    comments = relationship('Comment', back_populates='parent_post')
+    
+    
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.ForeignKey('users.id'))
+    comment_author = relationship('User', back_populates='comments')
+    
+    post_id = db.Column(db.ForeignKey('shitposts.id'))
+    parent_post = relationship('ShitPost', back_populates='comments')
+    text = db.Column(db.String(250), nullable=False)
