084a90ac5758cd9579f4d255747250d5dd94d849
Steven Lloyd Mandac <steven.mandac15@gmail.com>
2023-10-02 22:00:47 +0800

    [add] signup and login added

--- website/__init__.py
+++ website/__init__.py
@@ -1,19 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "shitpost.db"
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = "top-secret-key"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    login_manager.init_app(app)
     
     from .views import views
     from .auth import auth
     
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    
+    with app.app_context():
+        db.create_all()
     
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,60 @@
-from flask import Blueprint, render_template, redirect, url_for
-
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db, login_manager
+from .models import User
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        hashed_password = generate_password_hash(password, method="pbkdf2:sha256", salt_length=8)
+        try:
+            email_exist = db.session.query(User).where(User.email == email).scalar()
+            username_exist = db.session.query(User).where(User.username == username).scalar()
+        except:
+            pass
+        else:            
+            if username_exist:
+                flash("Username already taken.", category="danger")
+            elif email_exist:
+                flash("You have already signed up with this email, login instead!", category="danger")
+        
+        new_user = User(username=username,
+                        email=email,
+                        password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for('views.home'))
+    return render_template("signup.html")
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = db.session.query(User).where(User.username == username).scalar()
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user)
+                return redirect(url_for('views.home'))
+            else:
+                flash("Incorrect password.", category="danger")
+        else:
+            flash("User not registered.", category="danger")
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
+    logout_user()
     return redirect(url_for('views.home'))
-
-@auth.route('/sign-up')
-def signup():
-    return render_template("signup.html")
