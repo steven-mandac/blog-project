e9cb3394b7c2ff3442d8a8068176e39c6c72931c
Steven Lloyd Mandac <steven.mandac15@gmail.com>
2023-10-06 02:45:00 +0800

    create profile page

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from datetime import date
-from flask import Blueprint, render_template, request, redirect, flash
-from flask_login import current_user
+from flask import Blueprint, render_template, redirect, flash, url_for, request
+from flask_login import current_user, login_required
 from .models import User, ShitPost, Comment
 from .forms import ShitPostForm, CommentForm
 from . import db
@@ -19,6 +19,13 @@ def home():
         )
         db.session.add(new_post)
         db.session.commit()
-        flash("Shit posted!", category="success")
+        return redirect(url_for('views.home'))
     return render_template("index.html", shit_posts=posts, shit_form=shit_form)
 
+@views.route('/profile/<string:username>')
+@login_required
+def profile(username):
+    user = db.session.execute(db.select(User).where(User.username == username)).scalar()
+    posts = db.session.execute(db.select(ShitPost).where(ShitPost.user_id == user.id)).scalars().all()
+    return render_template("profile.html", user_posts=posts, user=username)
+    
